003f250f4522b8f73e7b22971a5fe1ad8cceb9d8
FCAS_FS <94762834+fcasfs-of@users.noreply.github.com>
2026-04-29 19:34:19 -0300

    Update api.js

--- app/api.js
+++ app/api.js
@@ -112,7 +112,7 @@ sidebar.classList.add('fullscreen'); }
 function initSidebar_oac(sidebar_obj, id){ 
 if(sidebar_obj && id && id!=""){ 
 	if(document.querySelector(id)){   document.querySelector(id).classList.remove("not-clickable");    }
-//sidebar_obj.innerHTML=sidebar_obj.innerHTML+"<style>  "+id+" { opacity:1 !important;  pointerevents:auto !important;   } </style>";
+sidebar_obj.innerHTML=sidebar_obj.innerHTML+"<style>  "+id+" { opacity:1 !important;  pointer-events:auto !important;   } </style>";
 }    }
 
 
